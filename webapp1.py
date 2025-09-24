7d77db2f83f452186052e7a3c8eeb5be7b5f9048
gkj-git <eaddress.gopi@gmail.com>
2025-09-24 18:34:55 -0400

    commit2

--- webapp1.py
+++ webapp1.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from GKJ  on Azure! 🚀"
+    return "Hello from GKJ  on Azure!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
